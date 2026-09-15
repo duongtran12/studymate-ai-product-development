76c7f6cf08109ef329339d8dccfc1bbd33dae505
Tran Viet Truong <tranviettruong013@gmail.com>
2026-09-15 08:41:44 +0700

    feat: add document library and upload UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import CourseOverviewPage from "./pages/CourseOverviewPage";
 import DashboardPage from "./pages/DashboardPage";
+import DocumentLibraryPage from "./pages/DocumentLibraryPage";
 import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import PlaceholderPage from "./pages/PlaceholderPage";
@@ -17,7 +18,8 @@ export default function App() {
           <Route index element={<DashboardPage />} />
           <Route path="courses" element={<DashboardPage />} />
           <Route path="courses/:courseId" element={<CourseOverviewPage />} />
-          <Route path="documents" element={<PlaceholderPage title="Tai lieu" description="Quan ly tai lieu dang xu ly va san sang de hoc." />} />
+          <Route path="documents" element={<DocumentLibraryPage />} />
+          <Route path="courses/:courseId/documents" element={<DocumentLibraryPage />} />
           <Route path="study-sessions" element={<PlaceholderPage title="Phien hoc" description="Hoi dap theo tai lieu va kiem tra nguon trich dan." />} />
           <Route path="quizzes" element={<PlaceholderPage title="Quiz" description="On tap voi cau hoi duoc tao tu tai lieu mon hoc." />} />
           <Route path="profile" element={<PlaceholderPage title="Ho so" description="Quan ly tai khoan va tuy chon hoc tap." />} />
