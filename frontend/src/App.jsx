607f94f1949797928818afdae7622e5660d56502
duongtran12 <tranducduong2204@gmail.com>
2026-09-19 18:11:37 +0700

    feat: add grounded chat interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import AppLayout from "./layouts/AppLayout";
 import AuthPage from "./pages/AuthPage";
 import DashboardPage from "./pages/DashboardPage";
 import DocumentLibraryPage from "./pages/DocumentLibraryPage";
+import StudySessionPage from "./pages/StudySessionPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import PlaceholderPage from "./pages/PlaceholderPage";
 
@@ -16,7 +17,7 @@ export default function App() {
           <Route index element={<DashboardPage />} />
           <Route path="courses" element={<PlaceholderPage title="Mon hoc" description="Tao va mo khong gian hoc theo tung mon." />} />
           <Route path="documents" element={<DocumentLibraryPage />} />
-          <Route path="study-sessions" element={<PlaceholderPage title="Phien hoc" description="Hoi dap theo tai lieu va kiem tra nguon trich dan." />} />
+          <Route path="study-sessions" element={<StudySessionPage />} />
           <Route path="quizzes" element={<PlaceholderPage title="Quiz" description="On tap voi cau hoi duoc tao tu tai lieu mon hoc." />} />
           <Route path="profile" element={<PlaceholderPage title="Ho so" description="Quan ly tai khoan va tuy chon hoc tap." />} />
           <Route path="*" element={<NotFoundPage />} />
